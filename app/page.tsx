eccd3a542988fe3d7b75f185d6642a78ac3501d5
Omar <annexomar@gmail.com>
2023-06-06 12:41:48 +0300

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,54 @@
+"use client";
+
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import ProductSection from "@/components/productsSection";
 
 export default function Home() {
-  return <></>;
+  return (
+    <main className="container w-full mx-auto md:w-5/6">
+      <section className="mt-32">
+        <h1 className="text-4xl font-bold text-center mt-2">
+          Best Gas refill station in Kenya
+        </h1>
+        <Swiper navigation={true} modules={[Navigation]}>
+          <SwiperSlide>
+            <div className="flex items-center gap-10">
+              <Image
+                src="/images/afrigas.png"
+                alt="afrigas"
+                width="300"
+                height="300"
+                className="rounded-lg"
+              />
+              <div className="flex flex-col gap-2">
+                <p className="text-3xl ml-4">Afrigas</p>
+                <p className="text-base ml-4">13KG gas</p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex items-center gap-10">
+              <Image
+                src="/images/kgas.png"
+                alt="kgas"
+                width="300"
+                height="300"
+                className="rounded-lg"
+              />
+              <div className="flex flex-col gap-2">
+                <p className="text-3xl ml-4">K-gas</p>
+                <p className="text-base ml-4">13KG gas</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </section>
+      <section className="mt-32">{/* <ProductSection /> */}</section>
+    </main>
+  );
 }
