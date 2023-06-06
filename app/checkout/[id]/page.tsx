0c22dcd074300c6ef1282130758affc9a994e5d9
Omar <annexomar@gmail.com>
2023-06-06 23:11:58 +0300

    feat: checkout page

---
+++ app/checkout/[id]/page.tsx
@@ -0,0 +1,262 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import useSWR from "swr";
+import axios from "axios";
+import {
+  AiOutlineCreditCard,
+  AiOutlineIdcard,
+  AiOutlineMail,
+} from "react-icons/ai";
+import fetcher from "@/utils/fetcher";
+import { GasProduct, Order } from "@prisma/client";
+import { Oval } from "react-loader-spinner";
+import { useRouter } from "next/navigation";
+
+const Checkout = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
+
+  const { data: gas, isLoading } = useSWR<GasProduct>(
+    `/api/product/${params.id}`,
+    fetcher
+  );
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post<Order>("/api/order", {
+        userId: "clikmzr5k0000d244w6lwoaqe",
+        total_amount: gas?.price,
+        productId: gas?.id,
+      });
+      router.push(`/success/${res.data.id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <section className="h-screen flex items-center gap-10 justify-center container mx-auto w-5/6">
+        <p className="my-32 text-3xl font-bold uppercase">Loading</p>
+        <Oval
+          height={80}
+          width={80}
+          color="#4fa94d"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#4fa94d"
+          strokeWidth={2}
+          strokeWidthSecondary={2}
+        />
+      </section>
+    );
+  }
+  return (
+    <section className="container mx-auto px-4 py-16">
+      <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+        <a href="#" className="text-2xl font-bold text-gray-800">
+          CheckOut
+        </a>
+      </div>
+      <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+        <div className="px-4 pt-8">
+          <p className="text-xl font-medium">Order Summary</p>
+          <p className="text-gray-400">
+            Check your items. And select a suitable shipping method.
+          </p>
+          <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
+            <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+              <img
+                className="m-2 h-24 w-28 rounded-md border object-cover object-center"
+                src={gas?.image}
+                alt=""
+              />
+              <div className="flex w-full flex-col px-4 py-4">
+                <span className="font-semibold uppercase">{gas?.name}</span>
+                <span className="float-right text-gray-400">{gas?.size}</span>
+                <p className="text-lg font-bold">Ksh {gas?.price}</p>
+              </div>
+            </div>
+          </div>
+
+          <p className="mt-8 text-lg font-medium">Shipping Methods</p>
+          <form className="mt-5 grid gap-6">
+            <div className="relative">
+              <input
+                className="peer hidden"
+                id="radio_1"
+                type="radio"
+                name="radio"
+                checked
+              />
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <label
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                htmlFor="radio_1"
+              >
+                <img
+                  className="w-14 object-contain"
+                  src="/images/bike.jpg"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">Bike Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
+                    Delivery: 5-10 mins
+                  </p>
+                </div>
+              </label>
+            </div>
+            <div className="relative">
+              <input
+                className="peer hidden"
+                id="radio_2"
+                type="radio"
+                name="radio"
+                checked
+              />
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <label
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                htmlFor="radio_2"
+              >
+                <img
+                  className="w-14 object-contain"
+                  src="/images/truck.jpg"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">Free Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
+                    Delivery: 1-2 hours
+                  </p>
+                </div>
+              </label>
+            </div>
+          </form>
+        </div>
+        <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
+          <p className="text-xl font-medium">Payment Details</p>
+          <p className="text-gray-400">
+            Complete your order by providing your payment details.
+          </p>
+          <div className="">
+            <label
+              htmlFor="email"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="your.email@gmail.com"
+              />
+              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                <AiOutlineMail />
+              </div>
+            </div>
+            <label
+              htmlFor="card-holder"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Card Holder
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="card-holder"
+                name="card-holder"
+                className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Your full name here"
+              />
+              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                <AiOutlineIdcard />
+              </div>
+            </div>
+            <label
+              htmlFor="card-no"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Card Details
+            </label>
+            <div className="flex">
+              <div className="relative w-7/12 flex-shrink-0">
+                <input
+                  type="text"
+                  id="card-no"
+                  name="card-no"
+                  className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="xxxx-xxxx-xxxx-xxxx"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                  <AiOutlineCreditCard />
+                </div>
+              </div>
+              <input
+                type="text"
+                name="credit-expiry"
+                className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="MM/YY"
+              />
+              <input
+                type="text"
+                name="credit-cvc"
+                className="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="CVC"
+              />
+            </div>
+            <label
+              htmlFor="billing-address"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Billing Address
+            </label>
+            <div className="flex flex-col sm:flex-row">
+              <div className="relative flex-shrink-0 sm:w-7/12">
+                <input
+                  type="text"
+                  id="billing-address"
+                  name="billing-address"
+                  className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Street Address"
+                />
+              </div>
+              <select
+                name="billing-state"
+                className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+              >
+                <option value="mombasa">Mombasa</option>
+              </select>
+            </div>
+            <div className="mt-6 border-t border-b py-2">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Subtotal</p>
+                <p className="font-semibold text-gray-900">Ksh {gas?.price}</p>
+              </div>
+            </div>
+            <div className="mt-6 flex items-center justify-between">
+              <p className="text-sm font-medium text-gray-900">Total</p>
+              <p className="text-2xl font-semibold text-gray-900">
+                Ksh {gas?.price}
+              </p>
+            </div>
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white"
+          >
+            Place Order
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Checkout;
