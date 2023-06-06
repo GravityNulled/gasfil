0a5df487bed4d7c9aa4527d4464ec1231c4e4fe7
Omar <annexomar@gmail.com>
2023-06-06 23:12:55 +0300

    feat: product list

--- components/productsSection.tsx
+++ components/productsSection.tsx
@@ -1,21 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import prisma from "@/utils/client";
 import Image from "next/image";
+import { GasProduct } from "@prisma/client";
+import fetcher from "@/utils/fetcher";
+import useSWR from "swr";
 
-const getGas = async () => {
-  const gases = await prisma.gasProduct.findMany();
-  return gases;
-};
-
-export default async function ProductSection() {
-  const gases = await getGas();
+export default function ProductSection() {
+  const { data: gases, isLoading } = useSWR<GasProduct[]>(
+    `/api/product`,
+    fetcher
+  );
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {gases.map((gas) => (
+          {gases?.map((gas) => (
             <a key={gas.id} href={`/products/${gas.id}`} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img
